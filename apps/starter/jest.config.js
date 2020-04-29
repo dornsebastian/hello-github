25080725db28c1c3ea0d7938c621dd6930a2fafa
Sebastian Dorn <36267720+dornsebastian@users.noreply.github.com>
2020-04-29 18:15:29 +0200

    initial commit

---
+++ apps/starter/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'starter',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/starter',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
